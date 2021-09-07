68362d7ac0baa8013f3a8ed2a96e51be5c28c962
99anime <89612530+99anime@users.noreply.github.com>
2021-09-07 13:57:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-if(window.location.href=="https://99anime.github.io/jsver/anime#attackontitan"){document.body.style.display ="none";}
-if(window.location.href=="https://99anime.github.io/jsver/anime.html#attackontitan"){document.body.style.display ="none";}
+if(window.location.href="https://99anime.github.io/jsver/anime.html?attackontitan"){window.location.assign("attackontitan.html")}
